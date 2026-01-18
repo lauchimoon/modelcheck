cf622f819ead6677a8b58dc5d7d822a5480c7b57
lauchimoon <laurentino.luna06@gmail.com>
2026-01-17 23:32:15 -0300

    Start implementing lexer
    Only lexes punctuation

--- src/main.rs
+++ src/main.rs
@@ -10,25 +10,71 @@ enum TokenKind {
 struct Token {
     kind: TokenKind,
     value: String,
+    line: usize,
+}
+
+struct Lexer {
+    src: Vec<char>,
+    src_len: usize,
+    cursor: usize,
+    line: usize,
 }
 
 fn main() {
-    let _src = "let S {s0,s1,s2,s3}".to_string();
-
-    let mut tokens: Vec<Token> = Vec::new();
-    tokens.push(Token{kind: TokenKind::Keyword, value: "let".to_string()});
-    tokens.push(Token{kind: TokenKind::Symbol, value: "S".to_string()});
-    tokens.push(Token{kind: TokenKind::OpenCurly, value: "{".to_string()});
-    tokens.push(Token{kind: TokenKind::Symbol, value: "s0".to_string()});
-    tokens.push(Token{kind: TokenKind::Comma, value: ",".to_string()});
-    tokens.push(Token{kind: TokenKind::Symbol, value: "s1".to_string()});
-    tokens.push(Token{kind: TokenKind::Comma, value: ",".to_string()});
-    tokens.push(Token{kind: TokenKind::Symbol, value: "s2".to_string()});
-    tokens.push(Token{kind: TokenKind::Comma, value: ",".to_string()});
-    tokens.push(Token{kind: TokenKind::Symbol, value: "s3".to_string()});
-    tokens.push(Token{kind: TokenKind::CloseCurly, value: "}".to_string()});
+    let src = "let S {s0,s1,s2,s3}".to_string();
+    let mut lexer = Lexer::new(src);
+    let tokens = lexer.lex();
 
     for tok in tokens {
-        println!("{:#?}: {}", tok.kind, tok.value);
+        println!("{:#?}: {} @ line {}", tok.kind, tok.value, tok.line);
+    }
+}
+
+impl Lexer {
+    fn new(src: String) -> Self {
+        return Lexer{
+            src: src.chars().collect(),
+            src_len: src.len(),
+            cursor: 0,
+            line: 1,
+        }
+    }
+
+    fn chop(&mut self) -> char {
+        if self.cursor >= self.src_len {
+            return ' ';
+        }
+        let c = self.src[self.cursor];
+        self.cursor += 1;
+        c
+    }
+
+    fn lex(&mut self) -> Vec<Token> {
+        let mut tokens: Vec<Token> = Vec::new();
+        while self.cursor < self.src_len {
+            let c = self.chop();
+            if c == '{' {
+                tokens.push(Token {
+                    kind: TokenKind::OpenCurly,
+                    value: "{".to_string(),
+                    line: self.line,
+                });
+            } else if c == '}' {
+                tokens.push(Token {
+                    kind: TokenKind::CloseCurly,
+                    value: "}".to_string(),
+                    line: self.line,
+                });
+            } else if c == ',' {
+                tokens.push(Token {
+                    kind: TokenKind::Comma,
+                    value: ",".to_string(),
+                    line: self.line,
+                });
+            } else if c == '\n' {
+                self.line += 1;
+            }
+        }
+        tokens
     }
 }
