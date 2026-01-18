9221116f815099a58ffb45f1130e70ac3d6a3ee5
lauchimoon <laurentino.luna06@gmail.com>
2026-01-18 19:59:43 -0300

    Implement reading src from a file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
 mod token;
 mod lexer;
 use lexer::Lexer;
+use std::fs;
+use std::env;
+use std::path::Path;
+use std::ffi::OsStr;
 
 fn main() {
-    let src = "let S {s0,s1,s2,s3}".to_string();
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        panic!("{}: missing .model file", args[0]);
+    }
+
+    let filepath = Path::new(&args[1]);
+    let ext = filepath.extension();
+    if ext != Some(OsStr::new("model")) {
+        panic!("{}: '{}' is not a .model file", args[0], filepath.display());
+    }
+
+    let src = fs::read_to_string(filepath).expect("Error opening file");
     let mut lexer = Lexer::new(src);
     let tokens = lexer.lex();
-
     for tok in tokens {
         println!("{:#?}: {} @ line {}", tok.kind, tok.value, tok.line);
     }
