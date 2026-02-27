9f4b3f100a77078d13b1a0e9574e060d488463a8
lauchimoon <laurentino.luna06@gmail.com>
2026-02-27 16:45:57 -0300

    Allow proposition to be inputted via command arguments

--- src/main.rs
+++ src/main.rs
@@ -8,32 +8,33 @@ use std::path::Path;
 use std::ffi::OsStr;
 
 fn main() {
-    let model = load_model();
+    let (model, prop) = load_model_and_prop();
     println!("S: {:#?}\nI: {:#?}", model.states, model.init_states);
     for (ident, state) in &model.state_info {
         println!("{}: Labels: {:#?}\nTransitions: {:#?}", ident, state.labels, state.transitions);
     }
-
-    let valid = model.check("E[~c U (b ^ ~t)]".to_string());
-    println!("{}", valid);
-}
-
-fn load_model() -> Model {
-    let filepath = parse_args();
-    Model::new(filepath)
-}
-
-fn parse_args() -> String {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        panic!("{}: missing .model file", args[0]);
+    let valid = model.check(prop.clone());
+    if valid {
+        println!("M |= {}", prop);
+    } else {
+        println!("M |/= {}", prop);
     }
+}
 
+fn load_model_and_prop() -> (Model, String) {
+    let (filepath, prop) = parse_args();
+    (Model::new(filepath), prop)
+}
+
+fn parse_args() -> (String, String) {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        panic!("{}: missing .model file and proposition", args[0]);
+    }
     let filepath = Path::new(&args[1]);
     let ext = filepath.extension();
     if ext != Some(OsStr::new("model")) {
         panic!("{}: '{}' is not a .model file", args[0], filepath.display());
     }
-
-    filepath.to_str().unwrap().to_string()
+    (filepath.to_str().unwrap().to_string(), args[2].clone())
 }
